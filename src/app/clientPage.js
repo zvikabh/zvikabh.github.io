783f2109ecfdeb63cdcd67b4a998ab2ffe811ef9
Zvika Ben-Haim <zvikabh@users.noreply.github.com>
2025-06-21 09:45:25 +0300

    Also add Zvika here

--- src/app/clientPage.js
+++ src/app/clientPage.js
@@ -13,7 +13,7 @@ export default function ClientPage() {
       <p>Welcome to Zvika's homepage!</p>
       <p>Stuff you can find here includes:</p>
       <ul>
-        <li><a href="/publications">My publications</a></li>
+        <li><a href="/publications">Zvika's publications</a></li>
         <li><a href="/software">Fun software projects</a></li>
       </ul>
     </>
